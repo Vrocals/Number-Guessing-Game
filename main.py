dd076fe29e7139f23b13dde0ce3e7abba473a1cc
Vrocals <116925727+Vrocals@users.noreply.github.com>
2022-10-29 14:55:53 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+GuessInput = int(input(f'Guess the number between 1 and: '))
+
+numberRandom = random.randint(1,int(f'{GuessInput}'))
+
+print(numberRandom)
+
+while GuessInput != numberRandom:
+
+    GuessNumber = int(input(f'Guess the number between 1 and {GuessInput}: '))
+    
+    if GuessNumber == numberRandom:
+            print('You Guessed the Number Right!')
+            break
+            
+
+    elif GuessNumber > numberRandom:
+            print('Your guess was too high')
+            
+
+
+    elif GuessNumber < numberRandom:
+            print('Your guess was too low')
+
+
+
+
+
+
